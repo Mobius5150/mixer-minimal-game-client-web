2f2c668dc7fbbc232aae9ce9feac11e8a4b71ee8
Mike Blouin <mblouin@microsoft.com>
2018-03-05 18:36:34 -0800

    Descriptive bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'minimal_game_client.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
